807544efd306b3f65e8d9dbb12c3ce0fce81f60c
Haisenberg <1paf1@ukr.net>
2023-12-29 23:59:24 +0200

    second task

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-a, b, c = int(input("Введіть число:")), int(input("Введіть число:")), int(input("Введіть число:"))
-print(max(a, b, c))
-print(min(a, b, c))
-print((a + b + c) / 3)
+
+meters = float(input("Введіть кількість метрів: "))
+choice = int(input("Оберіть одиницю вимірювання (1 - милі, 2 - дюйми, 3 - ярди): "))
+
+if choice == 1:
+    print(meters * 0.000621371192)
+elif choice == 2:
+    print(meters * 39.3700787)
+elif choice == 3:
+    print(meters * 1.0936133)
+else:
+    print("Невірний вибір одиниці вимірювання.")
